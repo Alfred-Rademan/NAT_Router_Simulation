b5919d61ca6de9aa4bc3397ff6d1b6d94a47f7d4
Alfred <rademanalfred@gmail.com>
2022-08-25 17:03:33 +0200

    Full DHCP work. TODO send last Ack and NAT Table

--- src/Client.py
+++ src/Client.py
@@ -1,20 +1,52 @@
+import ipaddress
 import scapy
 import socket
 import dhcppython
 
 mac_addr = 'AB:CD:BE:EF:C0:74'
 
-def send_DHCPConnect(clientsock:socket):
+def send_DHCPDisc(clientsock):
+
     dhcp_packet = dhcppython.packet.DHCPPacket.Discover(mac_addr)
     clientsock.sendto(dhcp_packet.asbytes,('<broadcast>',5000))
+    offer_wait(clientsock)
 
+def offer_wait(clientsock):
+
+    recv_packet, addr = clientsock.recvfrom(1024)
+    packet = dhcppython.packet.DHCPPacket.from_bytes(recv_packet)
+    dhcp_type = packet.options.as_dict()['dhcp_message_type']
+
+    if packet.op == 'BOOTREPLY' and dhcp_type == 'DHCPOFFER':
+        offer_ip = packet.yiaddr
+        rec_ID = packet.xid
+        send_Req(offer_ip, rec_ID, clientsock)
+
+def send_Req(offer_ip, rec_ID,clientsock):
+
+    packet = dhcppython.packet.DHCPPacket(op="BOOTREQUEST",
+    htype="ETHERNET",
+    hlen=6,
+    hops=0,
+    xid=rec_ID,
+    secs=0,
+    flags=0,
+    ciaddr=ipaddress.IPv4Address(0),
+    yiaddr=ipaddress.IPv4Address(offer_ip),
+    siaddr=ipaddress.IPv4Address(0),
+    giaddr=ipaddress.IPv4Address(0),
+    chaddr=mac_addr,
+    sname=b'',
+    file=b'',
+    options=dhcppython.options.OptionList([dhcppython.options.options.short_value_to_object(53, "DHCPREQUEST")]))
+    clientsock.sendto(packet.asbytes, ('<broadcast>', 5000))
 
 def main():
     clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     clientsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     clientsock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     clientsock.bind(('',4020))
-    send_DHCPConnect(clientsock)
+    send_DHCPDisc(clientsock)
 
 if __name__ == '__main__':
     main()
--- src/Server.py
+++ src/Server.py
@@ -1,19 +1,70 @@
+import ipaddress
 import scapy
 import socket
 import dhcppython
+import threading
 
-def recieve_DHCPConnect(server:socket):  
-    recv_packet = server.recv(1024)
+mac_addr = 'BB:CD:BE:EF:C0:34'
+ip_pool = []
+ip_assigned = []
+assigned_table = {}
+
+def ip_create(start, end):
+    for num in range(start,end):
+        ip_pool.append('10.0.0.' + str(num))
+
+def recieve_DHCPConnect(server:socket):
+
+    while True:
+
+        recv_packet, addr = server.recvfrom(1024)
+        packet = dhcppython.packet.DHCPPacket.from_bytes(recv_packet)
+        dhcp_type = packet.options.as_dict()['dhcp_message_type']
+        print(packet)
+        print(dhcp_type)
+
+        if packet.op == 'BOOTREQUEST' and dhcp_type == 'DHCPDISCOVER':
+            handle_Connect(packet,server)
+
+def connect(server):
+    recv_packet, addr = server.recvfrom(1024)
     packet = dhcppython.packet.DHCPPacket.from_bytes(recv_packet)
-    print(packet)
+    dhcp_type = packet.options.as_dict()['dhcp_message_type']
+    if packet.op == 'BOOTREQUEST' and dhcp_type == 'DHCPREQUEST':
+        print(packet.yiaddr)
 
+def handle_Connect(packet, server):
+
+    client_mac = packet.chaddr
+    rec_ID = packet.xid
+    offered_IP = None
+    i = 2
+
+    while offered_IP == None and ip_assigned.count != ip_pool.count :
+        test_IP = ip_pool[i]
+        if test_IP not in ip_assigned:
+            offered_IP = test_IP
+            ip_assigned.append(test_IP)
+        i += 1
+    
+    offer_packet = dhcppython.packet.DHCPPacket.Offer(mac_addr,0,rec_ID,
+    ipaddress.IPv4Address(offered_IP))
+    server.sendto(offer_packet.asbytes,('<broadcast>', 4020))
+    connect(server)    
+    
 
 def main():
+
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     server.bind(("", 5000))
-    recieve_DHCPConnect(server)
+
+    ip_create(1,11)
+    ip_assigned.append('10.0.0.1')
+
+    recieve_thread = threading.Thread(target=recieve_DHCPConnect,args= (server,))
+    recieve_thread.start()
 
 if __name__ == '__main__':
     main()
